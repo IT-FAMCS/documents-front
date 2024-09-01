9e6462b98e7520dc06492655ed7c4f910f3243b8
PERSEFA1 <epiatrovich@gamil.com>
2024-09-02 01:45:19 +0300

    feat:new db

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -4,3 +4,4 @@ export const MEMBERS_DELETE = "api/users/delete";
 export const LOGIN_URL = `${process.env.REACT_APP_HAND_URL}/api/users/login/`;
 export const LOGOUT_URL = `${process.env.REACT_APP_HAND_URL}/api/users/logout/`;
 export const CHECK_TOKEN_URL = `${process.env.REACT_APP_HAND_URL}/api/users/verify_token/`;
+export const REGISTER_URL = `${process.env.REACT_APP_HAND_URL}/api/users/registration/`;
